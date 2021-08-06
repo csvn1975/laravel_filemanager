44725544adea765f595f6edfc480cf0a1f1a8fdc
Nguyen Le <csvn1975khonnan@gmail.com>
2021-08-06 20:06:41 +0200

    add standard button

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,22 @@
 @extends('layouts.app')
 
 @push('page-css')
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
 <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
 <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.3/summernote.css" rel="stylesheet">
+<style>
+   #holder img {
+    margin-right: 10px;
+    margin-top: 30px;
+    box-shadow: 1px 1px 5px rgba(1,1,1,0.3);
+    };
+</style>
 @endpush
 
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -26,23 +36,31 @@
         </div>
     </div>
 
-    <div class="row mt-2">
+    <div class="row mt-5">
         <div class="col-md-8 offset-2">
             <div class="input-group">
                 <span class="input-group-btn">
-                    <a id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-primary">
+                    <button id="lfm" data-input="thumbnail" data-preview="holder" class="btn btn-danger">
                         <i class="fa fa-picture-o"></i> Choose
-                    </a>
+                    </button>
                 </span>
                 <input id="thumbnail" class="form-control" type="text" name="filepath">
             </div>
-            <img id="holder" style="margin-top:15px;max-height:100px;">
+        </div>
+
+    </div>
+
+    <div class="row">
+        <div class="col-md-8 offset-2">
+            <div class="mr-5" id="holder" style="height:100%;  max-height:100px;"> </div>
         </div>
     </div>
 
 
+
+
     <!-- Summernote -->
-    <div class="row mt-1">
+    <div class="row mt-5">
         <div class="col-md-8 offset-2">
             <div class="form-group">
                 <textarea class="form-control" id="summernote-editor" name="content"></textarea>
@@ -58,8 +76,9 @@
 <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.js"></script>
 <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
 
+<!-- defer  -->
 <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.3/summernote.js" defer></script>
-<script src="/vendor/laravel-filemanager/js/stand-alone-button.js"></script>
+<script src="/vendor/laravel-filemanager/js/stand-alone-button.js" defer></script>
 
 <!-- summernote config -->
 <script src="{{asset('main/js/sommernote.js')}}"></script>;
@@ -67,8 +86,9 @@
 
 <!--uplaod standard button -->
 <script>
-    var route_prefix = "url-to-filemanager";
-    $('#lfm').filemanager('image', {prefix: route_prefix});
+    $(document).ready(function() {
+        $('#lfm').filemanager('image');
+    });
 </script>
 
 
